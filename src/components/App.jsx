826abca045fbdf2b5a33887ae6322786d0d5474d
Sushma Deegoju <sushmadeegoju@Sushmas-MacBook-Air.local>
2024-02-03 14:19:44 -0500

    pushed login code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,40 @@
 import '../css/App.css'
-import { Routes, Route } from "react-router-dom";
-// import Home from './HomePage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './HomePage'
+import Login from './Login'
 
 function App() {
 
   return (
-    <div>
-      <header >
-        <div style={{ color: "white", fontWeight: "bold" }}>EasyHire</div>
-        <nav style={{ display: "flex", justifyContent:"space-evenly"}}>
-          <ul>
-            <li>Home</li>
-            <li>Events</li>
-            <li>Jobs</li>
-          </ul>
-        </nav>
-      </header>
-        {/* <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes> */}
+    <Router>
+      <div>
+        <header >
+          <div style={{ color: "white", fontWeight: "bold" }}>SheNetworks</div>
+          <nav style={{ display: "flex", justifyContent:"space-evenly"}}>
+            <ul>
+              <li>Home</li>
+              <li>Events</li>
+              <li>Jobs</li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+              <Route path="/" element={<Home />} /> 
+              <Route path="/login" element={<Login />} /> 
+          </Routes> 
         <footer>
-        <section>
-          <p> &copy; 2023 EasyHire, Inc</p>
-        </section>
-        <section>
-          <p> About Us | Directions | Contact Us</p>
-        </section>
-        <section>
-          <p> Privacy Policy </p>
-        </section>
-      </footer>
-  </div>  
+          <section>
+            <p> &copy; 2023 EasyHire, Inc</p>
+          </section>
+          <section>
+            <p> About Us | Directions | Contact Us</p>
+          </section>
+          <section>
+            <p> Privacy Policy </p>
+          </section>
+        </footer>
+    </div>  
+  </Router>
   )
 }
 
