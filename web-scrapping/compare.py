f9fe1b8c99a8297538c037fe340b3da3b3af4ae6
zainsyed30 <zainnsyed30@gmail.com>
2024-02-03 17:15:57 -0500

    edit to compare.py

--- web-scrapping/compare.py
+++ web-scrapping/compare.py
@@ -1,10 +1,11 @@
 import os
+import csv
 
 # Get the directory of the current script
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the relative path to the female.txt file
-relative_path = "female.txt"
+relative_path = "male.txt"
 
 # Construct the full path
 file_path = os.path.join(script_directory, relative_path)
@@ -14,9 +15,7 @@ if os.path.isfile(file_path):
     # Open and read the file
     with open(file_path, "r") as file:
         female_data = file.read()
-        
-    # Print or use the file content
-    print(female_data)
-else:
-    # debugging
-    print(f"Error: File {relative_path} not found.")
+
+with open("data_linkedin.csv", 'r') as file1:
+    csvread = csv.reader(file1)
+    
