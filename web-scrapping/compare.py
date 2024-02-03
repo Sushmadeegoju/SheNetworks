aa249679288a7b276b33527ae2a32859cee3fe07
zainsyed30 <zainnsyed30@gmail.com>
2024-02-03 15:24:50 -0500

    adding webscrapping

---
+++ web-scrapping/compare.py
@@ -0,0 +1,7 @@
+import filecmp
+
+# reads female.txt file 
+# (https://www.cs.cmu.edu/Groups/AI/areas/nlp/corpora/names/female.txt)
+with open("/Users/zainsyed/Documents/hackViolet/SheNetworks/web-scrapping/female.txt", "r") as file:
+    femaleData = file.read()
+
